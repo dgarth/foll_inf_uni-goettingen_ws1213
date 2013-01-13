852b487d9b3dea3c96af3c6c34da965fa0d23c86
Robin Martinjak <rob@rmartinjak.de>
2013-01-13 17:47:38 +0100

    LcdControl.h: added macro "lcdprintf"

--- LcdControl/LcdControl.h
+++ LcdControl/LcdControl.h
@@ -15,4 +15,29 @@ enum
     LCD_BUTTON2     = 0x12,
 };
 
+
+#include <stdarg.h>
+#include <stdio.h>
+
+#define BUFFERS 3
+static char *_lcdprintf(const char *fmt, ...)
+{
+    /* use LCD_LEN+1 because vsnprintf adds a null byte */
+    static char buf[LCD_LEN+1][BUFFERS];
+    static int i = 0;
+
+    va_list ap;
+
+    i = (i+1) % BUFFERS;
+
+    va_start(ap, fmt);
+    vsnprintf(buf[i], sizeof buf[i], fmt, ap);
+    va_end(ap);
+
+    return buf[i];
+}
+#undef BUFFERS
+
+#define lcdprintf(...) puts(_lcdprintf(__VA_ARGS__))
+
 #endif
