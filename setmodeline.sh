54deeb48fb01571bacf1714588fff79ecdf98a57
Robin Martinjak <rob@rmartinjak.de>
2013-01-23 11:59:27 +0100

    setmodeline.sh: fixed missing space in modeline

--- setmodeline.sh
+++ setmodeline.sh
@@ -2,5 +2,5 @@
 modeline='filetype=nc:tabstop=4:expandtab:shiftwidth=0:softtabstop=-1'
 
 for f in $(find -regextype posix-extended -regex '.*\.(h|nc)'); do
-	sed -i "1d;2i //vim: ${modeline}" $f
+	sed -i "1d;2i // vim: ${modeline}" $f
 done
