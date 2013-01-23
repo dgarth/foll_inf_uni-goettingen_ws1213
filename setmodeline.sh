947e348479f4941fe11b61e266fb56ff7c9cac1d
Robin Martinjak <rob@rmartinjak.de>
2013-01-23 12:36:18 +0100

    modify setmodeline.sh so vim doesn't nag

--- setmodeline.sh
+++ setmodeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-modeline='filetype=nc:tabstop=4:expandtab:shiftwidth=0:softtabstop=-1'
+modeline='vim: filetype=nc:tabstop=4:expandtab:shiftwidth=0:softtabstop=-1'
 
 for f in $(find -regextype posix-extended -regex '.*\.(h|nc)'); do
-	sed -i "1d;2i // vim: ${modeline}" $f
+	sed -i "1d;2i // ${modeline}" $f
 done
