0a521733dfdb735850e45796c2dd3fc0aecf5fcf
Robin Martinjak <rob@rmartinjak.de>
2013-01-28 16:51:10 +0100

    setmodeline.sh: only set modeline for passed args

--- setmodeline.sh
+++ setmodeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 modeline='vim: filetype=nc:tabstop=4:expandtab:shiftwidth=0:softtabstop=-1'
 
-for f in $(find -regextype posix-extended -regex '.*\.(h|nc)'); do
+for f in $@; do
 	sed -i "1d;2i // ${modeline}" $f
 done
